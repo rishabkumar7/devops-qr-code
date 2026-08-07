ff3d18c1f9a11345027239e82581d763de0b8d80
ali <asek_23.02.09@mail.ru>
2026-08-07 12:21:24 +0500

    cicd

--- api/main.py
+++ api/main.py
@@ -18,6 +18,7 @@ origins = ["http://localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
